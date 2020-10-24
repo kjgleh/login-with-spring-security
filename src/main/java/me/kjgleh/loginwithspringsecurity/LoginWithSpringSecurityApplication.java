4a62ae5bb46ff9e595333677f68c8301e1def7c1
김정근 <gogoogjg@tripstore.kr>
2020-10-24 14:11:44 +0900

    initialize project

---
+++ src/main/java/me/kjgleh/loginwithspringsecurity/LoginWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package me.kjgleh.loginwithspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithSpringSecurityApplication.class, args);
+    }
+
+}
